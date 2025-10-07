1a8fa15de84bbb092b6432164e6ee652cbc3d683
Safison <wadalsafi2015@gmail.com>
2025-10-07 10:10:35 +0200

    added unit test for parquet file obfuscation

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,13 +4,16 @@ from src.utils import (
     parse_input_json,
     read_csv_from_s3,
     obfuscate_pii,
-    write_obfuscated_file_to_s3,
+    write_csv_obfuscated_file_to_s3,
+    read_parquet_from_s3,
+    write_parquet_obfuscated_file_to_s3,
 )
 import moto
 import boto3
 from moto import mock_aws
 import io
 from io import StringIO
+import csv
 
 
 @pytest.fixture
@@ -125,8 +128,28 @@ def test_obfuscate_no_df():
     assert obfuscated_df == "no data frame provided"
 
 
+# def test_obfuscated_csv_file():
+#     data = {
+#         "name": ["Anas", "Bob"],
+#         "email_address": ["anas@example.com", "bob@example.com"],
+#         "age": [22, 21],
+#         "cohort": [2023, 2024],
+#     }
+#     df = pd.DataFrame(data)
+#     pii_fields = ["name", "email_address"]
+#     df_obfuscate = obfuscate_pii(df, pii_fields)
+#     print(df_obfuscate)
+#     csv_file = obfuscated_csv_file(df_obfuscate)
+#     #print(csv_content)
+#     expected_content = (
+#         "name,email_address,age,cohort\r\n"
+#         "***,***,22,2023"
+#         "\r\n***,***,21,2024\r\n"
+#     )
+#     #assert csv_content.replace('\r\n', '\n') == expected_content.replace('\r\n', '\n')
+
 @mock_aws
-def test_write_obfuscated_file_to_s3(s3_client):
+def test_write_csv_obfuscated_file_to_s3(s3_client):
     # Create a mock S3 bucket
     bucket = "test-bucket"
     file_key = "test.csv"
@@ -138,18 +161,19 @@ def test_write_obfuscated_file_to_s3(s3_client):
         "cohort": [2023, 2024],
     }
     df = pd.DataFrame(data)
-    csv_file_key = write_obfuscated_file_to_s3(bucket, file_key, df)
+    df_obfuscate = obfuscate_pii(df, ["name", "email_address"])
+    csv_file_key = write_csv_obfuscated_file_to_s3(bucket, file_key, df_obfuscate)
     response = s3_client.get_object(Bucket=bucket, Key=csv_file_key)
     content = response["Body"].read().decode("utf-8")
     expected_content = (
         "name,email_address,age,cohort\r\n"
-        "Anas,anas@example.com,22,2023"
-        "\r\nBob,bob@example.com,21,2024\r\n"
+        "***,***,22,2023"
+        "\r\n***,***,21,2024\r\n"
     )
     assert content.replace('\r\n', '\n') == expected_content.replace('\r\n', '\n')
 
 
-def test_write_obfuscated_file_to_s3_invalid_bucket():
+def test_write_csv_obfuscated_file_to_s3_invalid_bucket():
     bucket_name = "nonexistent-bucket"
     file_key = "test.csv"
     data = {
@@ -160,10 +184,10 @@ def test_write_obfuscated_file_to_s3_invalid_bucket():
     }
     df = pd.DataFrame(data)
     with pytest.raises(Exception):
-        write_obfuscated_file_to_s3(bucket_name, file_key, df)
+        write_csv_obfuscated_file_to_s3(bucket_name, file_key, df)
     
 
-def test_write_obfuscated_file_no_file_key():
+def test_write_csv_obfuscated_file_no_file_key():
     bucket_name = "test-bucket"
     file_key = ""
     data = {
@@ -173,17 +197,17 @@ def test_write_obfuscated_file_no_file_key():
         "cohort": [2023, 2024],
     }   
     df = pd.DataFrame(data)
-    assert write_obfuscated_file_to_s3(bucket_name, file_key, df) == ("No file key provided")
+    assert write_csv_obfuscated_file_to_s3(bucket_name, file_key, df) == ("No file key provided")
 
 
-def test_write_obfuscated_file_to_s3_no_df():
+def test_write_csv_obfuscated_file_to_s3_no_df():
     bucket_name = "test-bucket"
     file_key = "test.csv"
     df = "no df"
-    assert write_obfuscated_file_to_s3(bucket_name, file_key, df) == ("No valid dataframe provided")
+    assert write_csv_obfuscated_file_to_s3(bucket_name, file_key, df) == ("No valid dataframe provided")
 
 
-def test_write_obfuscated_file_to_s3_no_csv_extension():
+def test_write_csv_obfuscated_file_to_s3_no_csv_extension():
     bucket_name = "test-bucket"
     file_key = "test.txt"
     data = {
@@ -193,10 +217,10 @@ def test_write_obfuscated_file_to_s3_no_csv_extension():
         "cohort": [2023, 2024],
     }
     df = pd.DataFrame(data)
-    assert write_obfuscated_file_to_s3(bucket_name, file_key, df) == ("File key must end with .csv")
+    assert write_csv_obfuscated_file_to_s3(bucket_name, file_key, df) == ("File key must end with .csv")
 
 
-def test_write_obfuscated_file_to_s3_no_bucket():
+def test_write_csv_obfuscated_file_to_s3_no_bucket():
     bucket_name = ""
     file_key = "test.csv"
     data = {
@@ -206,4 +230,49 @@ def test_write_obfuscated_file_to_s3_no_bucket():
         "cohort": [2023, 2024],
     }
     df = pd.DataFrame(data)
-    assert write_obfuscated_file_to_s3(bucket_name, file_key, df) == ("No bucket name provided")
+    assert write_csv_obfuscated_file_to_s3(bucket_name, file_key, df) == ("No bucket name provided")
+
+
+@mock_aws
+def test_read_parquet_from_s3(s3_client):
+    # Create a mock S3 bucket and upload a test parquet file
+    bucket_name = "test-bucket"
+    file_key = "students.parquet"
+    df_parq = pd.read_parquet("students.parquet")
+
+    parq_buffer = io.BytesIO()
+    df_parq.to_parquet(parq_buffer, engine="pyarrow", index=False)
+    parq_buffer.seek(0)
+
+    s3_client.create_bucket(Bucket=bucket_name)
+    s3_client.put_object(Bucket=bucket_name, Key=file_key, Body=parq_buffer.getvalue())
+    df = read_parquet_from_s3(bucket_name, file_key)
+    assert not df.empty
+    assert list(df.columns) == ["student_id","name","course","cohort","graduation_date","email_address"]
+    
+
+
+@mock_aws
+def test_write_parquet_obfuscated_file_to_s3(s3_client):
+    bucket_name = "test-bucket"
+    file_key = "students.parquet"
+    s3_client.create_bucket(Bucket=bucket_name)
+    data = {
+        "name": ["Anas", "Bob"],
+        "email_address": ["anas@example.com", "bob@example.com"],
+        "age": [22, 21],
+        "cohort": [2023, 2024],
+    }
+    
+    df = pd.DataFrame(data)
+    df_obfuscate = obfuscate_pii(df, ["name", "email_address"])
+    parquet_file_key = write_parquet_obfuscated_file_to_s3(bucket_name, file_key, df_obfuscate)
+    response = s3_client.get_object(Bucket=bucket_name, Key=parquet_file_key)
+    df = pd.read_parquet(io.BytesIO(response['Body'].read()))
+    assert not df.empty
+    assert list(df.columns) == ["name", "email_address", "age", "cohort"]
+    name_list = df["name"].tolist()
+    email_list = df["email_address"].tolist()
+    assert name_list == ["***", "***"]
+    assert email_list == ["***", "***"]
+    
