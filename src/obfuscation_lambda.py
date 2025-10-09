7f002b18b3991e735512a64012fba1c39119f428
Safison <wadalsafi2015@gmail.com>
2025-10-09 10:47:13 +0200

    update the lambda handler function and 'utils' script

--- src/obfuscation_lambda.py
+++ src/obfuscation_lambda.py
@@ -1,4 +1,5 @@
 import boto3
+import pandas as pd
 from utils import (
     parse_input_json,
     read_csv_from_s3,
@@ -13,41 +14,123 @@ from utils import (
 s3_client = boto3.client("s3")
 
 def lambda_handler(event, context):
-    """This function is triggered by an even bridge, step machine, etc.
-    files to obfuscate and pii fields are passed as an event arguments
-    It reads the CSV file, obfuscates sensitive data,
-    and saves the obfuscated file back to the S3 bucket."""
+    """This function is triggered by an event bridge, step machine, etc. 
+    the function obfuscates sensitive data in files stored in S3 bucket.
+    file to obfuscate is passed as s3 uri, along with the fields to be obfuscated 
+    as a json string.e.g:
+    {
+         "file_to_obfuscate": "s3://my_bucket/file_key.csv",
+         "pii_fields": ["field_1", "field_2"]
+    }
+  
+    It reads the file from s3 bucket, check its type by checking its extension, 
+    obfuscates sensitive data in the specified fields in the file, 
+    saves the obfuscated file back to S3 bucket, and returns the 
+    S3 URI of the obfuscated file. Supported file types are CSV, Parquet, and JSON."""
+    """For GDPR compliance:
+    - The obfuscation tool performs irreversible anonymization.
+    - No lookup tables or re-identification keys are retained.
+    - Logs and debug output never capture original data."""
+
     bucket_name, file_key, pii_fields = parse_input_json(event)
+    if bucket_name == "Input JSON is empty.":
+        return {
+        "statusCode": 400,
+        "body": "Input JSON is empty.",
+        }
+    if bucket_name == "bucket or file key is missing":
+        return {
+        "statusCode": 400,
+        "body": "No bucket name or file key provided",
+        }
+    if bucket_name == "Invalid S3 URI format":
+        return {
+        "statusCode": 400,
+        "body": "Invalid S3 URI format. Expected format: s3://bucket_name/file_key",
+        }
+    if file_key == "Unsupported file type":
+        return {
+        "statusCode": 400,
+        "body": "Unsupported file type. Only CSV, Parquet, and JSON files are supported.",
+        }
+    if pii_fields == ["No pii fields provided"]:
+        return {
+        "statusCode": 400,
+        "body": "No PII fields provided for obfuscation.",
+        }
+    # CSV file obfuscation
     if file_key.endswith(".csv"):
         df_csv = read_csv_from_s3(bucket_name, file_key, s3_client)
+        if not isinstance(df_csv, (pd.DataFrame)) and df_csv.startswith("Error"):
+            return {
+            "statusCode": 400,
+            "body": f"Error, no such file, specified key does not exist",
+            }
+        
         df_obfuscate = obfuscate_pii(df_csv, pii_fields)
+        
         obfus_file_key = write_csv_obfuscated_file_to_s3(bucket_name, file_key, df_obfuscate,s3_client)
+        if obfus_file_key.startswith("Error"):
+            return {
+            "statusCode": 400,
+            "body": f"Error writing obfuscated file to S3",
+            }
         return {
         "statusCode": 200,
-        "body": f"{file_key} Obfuscated version written to {bucket_name}/{obfus_file_key} successfully.",
+        "body": f"s3://{bucket_name}/{obfus_file_key}",
         }
-        
+    # Parquet file obfuscation
     elif file_key.endswith(".parquet"):
         df_parquet = read_parquet_from_s3(bucket_name, file_key,s3_client)
+        if not isinstance(df_parquet, (pd.DataFrame)) and df_parquet.startswith("Error"):
+            return {
+            "statusCode": 400,
+            "body": f"Error, no such file, specified key does not exist",
+            }
+        
         df_obfuscate = obfuscate_pii(df_parquet, pii_fields)
+        
         obfus_file_key = write_parquet_obfuscated_file_to_s3(bucket_name, file_key, df_obfuscate,s3_client)
+        if obfus_file_key.startswith("Error"):
+            return {
+            "statusCode": 400,
+            "body": f"Error writing obfuscated file to S3",
+            }
+        
         return {
         "statusCode": 200,
-        "body": f"{file_key} Obfuscated version written to {bucket_name}/{obfus_file_key} successfully.",
+        "body": f"s3://{bucket_name}/{obfus_file_key}",
         }
+    # JSON file obfuscation
     elif file_key.endswith(".json"):
         df_json = read_json_from_s3(bucket_name, file_key,s3_client)
+        if not isinstance(df_json, (pd.DataFrame)) and df_json.startswith("Error"):
+            return {
+            "statusCode": 400,
+            "body": f"Error, no such file, specified key does not exist",
+            }
+        
         obfuscated_df = obfuscate_pii(df_json, pii_fields)
+        
         obfus_file_key = write_json_obfuscated_file_to_s3(bucket_name, file_key, obfuscated_df,s3_client)
-        #print(f"{file_key} Obfuscated version written to {bucket_name}{obfus_file_key} successfully.")
+        if obfus_file_key.startswith("Error"):
+            return {
+            "statusCode": 400,
+            "body": f"Error writing obfuscated file to S3",
+            }
+        
         return {
         "statusCode": 200,
-        "body": f"{file_key} Obfuscated version written to {bucket_name}/{obfus_file_key} successfully.",
+        "body": f"s3://{bucket_name}/{obfus_file_key}",
         }
+    
+    
 ###################################
-# Local Testing Only
+#Local Testing Only
 # if __name__ == "__main__":
-#     lambda_handler({
-#          "file_to_obfuscate": "s3://ans-gdpr-bucket/students.json",
-#          "pii_fields": ["student_id", "email_address"]
+#     response = lambda_handler({
+#          "file_to_obfuscate": "s3://ans-gdpr-bucket/students.csv",
+#          "pii_fields": ["name", "email_address"]
 #      }, None)
+#     print(response)
+###################################
--- src/utils.py
+++ src/utils.py
@@ -5,24 +5,33 @@ from io import StringIO
 import pandas as pd
 
 
-
 def parse_input_json(input_json):
     """Parses the input JSON to extract bucket name,
     file key, and PII fields"""
     try:
         if input_json == {}:
-            return "", "", []
-        file_to_obfuscate = input_json["file_to_obfuscate"]
-        if not file_to_obfuscate.startswith("s3://") or file_to_obfuscate.count("/") < 2:
-            return "", "", []
+            return "Input JSON is empty.", "", []
         else:
-            bucket_name = file_to_obfuscate[5 : file_to_obfuscate.index("/", 5)]
-            file_key = file_to_obfuscate[file_to_obfuscate.index("/", 5) + 1 :]
-            if input_json.get("pii_fields") is None:
-                pii_fields = []
+            file_to_obfuscate = input_json["file_to_obfuscate"]
+            if file_to_obfuscate is None or file_to_obfuscate == "":
+                return "bucket or file key is missing", "", []
+            if file_to_obfuscate and not file_to_obfuscate.startswith("s3://") or file_to_obfuscate.count("/") < 2:
+                return "Invalid S3 URI format", "", []
+            if not (file_to_obfuscate.endswith(".csv") or file_to_obfuscate.endswith(".parquet") or file_to_obfuscate.endswith(".json")):
+                return "", "Unsupported file type", []
             else:
-                pii_fields = input_json["pii_fields"]
-            return bucket_name, file_key, pii_fields
+                bucket_name = file_to_obfuscate[5 : file_to_obfuscate.index("/", 5)]
+                file_key = file_to_obfuscate[file_to_obfuscate.index("/", 5) + 1 :]
+                if input_json.get("pii_fields") is None:
+                    pii_fields = ["No pii fields provided"]
+                    return bucket_name, file_key, pii_fields
+                elif not isinstance(input_json["pii_fields"], list) or len(input_json["pii_fields"]) == 0:
+                    pii_fields = ["No pii fields provided"]
+                    return bucket_name, file_key, pii_fields
+
+                else:
+                    pii_fields = input_json["pii_fields"]
+                return bucket_name, file_key, pii_fields
     except Exception as e:
         print(f"Error parsing input JSON: {e}")
         return "", "", []
@@ -49,13 +58,8 @@ def obfuscate_pii(df, pii_fields):
 def read_csv_from_s3(bucket_name, file_key,s3):
     """Reads a CSV file from an S3 bucket and returns
     its content as a pandas DataFrame"""
-    #s3 = boto3.client("s3")
     bucket_name = bucket_name
     file_key = file_key
-    if bucket_name == "" or file_key == "":
-        return "no bucket name or file key provided"  
-    elif not file_key.endswith(".csv"):
-        return "file is not a csv file"  
     try:
         obj = s3.get_object(Bucket=bucket_name, Key=file_key)
         csv_data = obj["Body"].read().decode("utf-8")
@@ -63,35 +67,21 @@ def read_csv_from_s3(bucket_name, file_key,s3):
         df_csv = pd.read_csv(csv_io)  
         return df_csv
     except Exception as e:
-        print(f"Error reading CSV from S3: {e}")
-        raise e
+        return (f"Error reading CSV from S3: {e}")
     
  
 def write_csv_obfuscated_file_to_s3(bucket_name, file_key, df,s3):
-    """Writes the obfuscated dataframe back to an S3 bucket as a CSV file"""
-    if bucket_name == "":
-        return ("No bucket name provided")
-    if file_key == "":
-        return ("No file key provided")    
-
-    if not isinstance(df, pd.DataFrame):
-        return ("No valid dataframe provided")
-        
-    if not file_key.endswith(".csv"):
-        return ("File key must end with .csv")
+    """Writes the obfuscated dataframe back to an S3 bucket as a CSV file and returns file key"""
     try:
         timestamp = pd.Timestamp.now().strftime("%Y%m%d%H%M%S")
         obfuscated_file_key = file_key.replace(".csv", "_obfuscated.csv")
         file_key = f"csv_files/{timestamp}_{obfuscated_file_key}"
-        #s3 = boto3.client("s3")
         csv_buffer = StringIO()
         df.to_csv(csv_buffer, index=False)
         s3.put_object(Bucket=bucket_name, Key=file_key, Body=csv_buffer.getvalue())
-        #print(f"CSV Obfuscated file written to s3://{bucket_name}/{file_key}")
         return file_key
     except Exception as e:
-        print(f"Error writing obfuscated file to S3: {e}")
-        raise e
+        return ("Error writing obfuscated file to S3")
 
 #######################
 #parquet file processing
@@ -104,47 +94,30 @@ def read_parquet_from_s3(bucket_name, file_key,s3):
         #s3 = boto3.client("s3")
         bucket_name = bucket_name
         file_key = file_key
-        if bucket_name == "" or file_key == "":
-            return "no bucket n]ame or file key provided"  
-        elif not file_key.endswith(".parquet"):
-            return "file is not a parquet file"
         try:
             obj = s3.get_object(Bucket=bucket_name, Key=file_key)
             parquet_data = obj['Body'].read()
             df_parquet = pd.read_parquet(io.BytesIO(parquet_data))
             return df_parquet
         except Exception as e:
-            print(f"Error reading Parquet from S3: {e}")
-            raise e
+            return (f"Error reading parquet from S3: {e}")
 
 
 def write_parquet_obfuscated_file_to_s3(bucket_name, file_key, df,s3):
-    """Writes the obfuscated dataframe back to an S3 bucket as a parquet file"""
-    if bucket_name == "":
-        return ("No bucket name provided")
-    if file_key == "":
-        return ("No file key provided")    
-
-    if not isinstance(df, pd.DataFrame):
-        return ("No valid dataframe provided")
-        
-    if not file_key.endswith(".parquet"):
-        return ("File key must end with .parquet")
+    """Writes the obfuscated dataframe back to an S3 bucket as a parquet file
+    and returns file key"""
     try:
         obfuscated_file_key = file_key.replace(".parquet", "_obfuscated.parquet")
         file_key = obfuscated_file_key
-        #s3 = boto3.client("s3")
         parq_buffer = io.BytesIO()
         df.to_parquet(parq_buffer, engine="pyarrow", index=False)
         parq_buffer.seek(0)
         time_stamp = pd.Timestamp.now().strftime("%Y%m%d%H%M%S")
         parq_file_key = f"parq_files/{time_stamp}_{file_key}"
         s3.put_object(Bucket=bucket_name, Key= parq_file_key, Body=parq_buffer.getvalue())
-        #print(f"Obfuscated file written to s3://{bucket_name}/{parq_file_key}")
         return parq_file_key
     except Exception as e:
-        print(f"Error writing obfuscated file to S3: {e}")
-        raise e
+        return (f"Error writing obfuscated parquet file to s3: {e}")
 
 
 ######################
@@ -153,52 +126,33 @@ def write_parquet_obfuscated_file_to_s3(bucket_name, file_key, df,s3):
 def read_json_from_s3(bucket_name, file_key,s3):
     """Reads a JSON file from an S3 bucket and returns
     its content as a pandas DataFrame"""
-    #s3 = boto3.client("s3")
     bucket_name = bucket_name
     file_key = file_key
-    if bucket_name == "" or file_key == "":
-        return "no bucket name or file key provided"  
-    elif not file_key.endswith(".json"):
-        return "file is not a json file"  
     try:
         obj = s3.get_object(Bucket=bucket_name, Key=file_key)
         json_data = obj["Body"].read().decode("utf-8")
         df_json = pd.read_json(StringIO(json_data))  
         return df_json
     except Exception as e:
-        print(f"Error reading JSON from S3: {e}")
-        raise e
+        return (f"Error reading json from S3: {e}")
     
 def write_json_obfuscated_file_to_s3(bucket_name, file_key, df,s3):
-    """Writes the obfuscated dataframe back to an S3 bucket as a JSON file"""
-    if bucket_name == "":
-        return ("No bucket name provided")
-    if file_key == "":
-        return ("No file key provided")    
-
-    if not isinstance(df, pd.DataFrame):
-        return ("No valid dataframe provided")
-        
-    if not file_key.endswith(".json"):
-        return ("File key must end with .json")
+    """Writes the obfuscated dataframe back to an S3 bucket as a JSON file
+    and returns file key"""
     try:
         timestamp = pd.Timestamp.now().strftime("%Y%m%d%H%M%S")
         obfuscated_file_key = file_key.replace(".json", "_obfuscated.json")
         file_key = f"json_files/{timestamp}_{obfuscated_file_key}"
-        #s3 = boto3.client("s3")
         json_buffer = StringIO()
         df.to_json(json_buffer, orient="records", lines=True)
         s3.put_object(Bucket=bucket_name, Key=file_key, Body=json_buffer.getvalue())
-        #print(f"JSON Obfuscated file written to s3://{bucket_name}/{file_key}")
         return file_key
     except Exception as e:
-        print(f"Error writing obfuscated file to S3: {e}")
-        raise e
+        return (f"Error writing obfuscated file to S3: {e}")
 
 
 ######################
 # Function developed For testign purposes only
-######################
 def convert_csv_to_parquet():
     """For testing puposes, the function converts a CSV file to a Parquet file"""
     try:
@@ -214,13 +168,14 @@ def convert_csv_to_parquet():
 # if __name__ == "__main__":
 #     s3 = boto3.client("s3")
 #     bucket_name, file_key,pii_fields = parse_input_json({
-#          "file_to_obfuscate": "s3://ans-gdpr-bucket/students.json",
-#          "pii_fields": ["name", "email_address"]
+#          "file_to_obfuscate": "s3://ans-gdpr-bucket/studentss.json",
+#          "pii_fields": []
 #       })
 #     if file_key.endswith(".csv"):
 #         df_csv = read_csv_from_s3(bucket_name, file_key,s3)
 #         obfuscated_df = obfuscate_pii(df_csv, pii_fields)
-#         write_csv_obfuscated_file_to_s3(bucket_name, file_key, obfuscated_df,s3)
+#         obs_file_key = write_csv_obfuscated_file_to_s3(bucket_name, file_key, obfuscated_df,s3)
+#         print(f"Obfuscated file written to s3://{bucket_name}/{obs_file_key}")
 #     elif file_key.endswith(".parquet"):
 #         df_parquet = read_parquet_from_s3(bucket_name, file_key,s3)
 #         obfuscated_df = obfuscate_pii(df_parquet, pii_fields)
