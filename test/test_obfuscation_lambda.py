6496a7296fdf3911a343ea6db10de2933e3bde08
Safison <wadalsafi2015@gmail.com>
2025-10-11 11:21:11 +0200

    added unit test for lambda handler for csv file

--- test/test_obfuscation_lambda.py
+++ test/test_obfuscation_lambda.py
@@ -19,14 +19,14 @@ def s3_client():
 
 @mock_aws
 def test_lambda_handler_csv(s3_client):
-    #s3_client = boto3.client("s3", region_name="us-east-1")
     bucket_name = "test-bucket"
     file_key = "test.csv"
     s3_client.create_bucket(Bucket=bucket_name)
     
-    csv_data = "name,email_address,age,cohort\r\nAnas,anas@example.com,22,2023\r\nBob,bob@example.com,21,2024\r\n"
+    csv_data = "name,email_address,age,cohort\nAnas,anas@example.com,22,2023\nBob,bob@example.com,21,2024\n"
     
     s3_client.put_object(Bucket=bucket_name, Key=file_key, Body=csv_data)
+    
     input_event = {
         "file_to_obfuscate": f"s3://{bucket_name}/{file_key}",
         "pii_fields": ["name", "email_address"]
@@ -35,6 +35,7 @@ def test_lambda_handler_csv(s3_client):
     print(response)
     #print(response["body"])
     assert response["statusCode"] == 200
+    
     obfus_file_key = response["body"].replace(f"s3://{bucket_name}/", "")
     #print(obfus_file_key)
     obj = s3_client.get_object(Bucket=bucket_name, Key=obfus_file_key)
